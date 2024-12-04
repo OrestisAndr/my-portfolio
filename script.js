856b4f00ff2291ca489bdbe6beef6544cfe5bf99
Orestis Androulakis <114134979+OrestisAndr@users.noreply.github.com>
2024-12-04 10:25:54 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Parallax Effect για την αρχική σελίδα
-window.addEventListener('scroll', function() {
-    const home = document.getElementById('home');
-    const scrollPosition = window.scrollY;
-    home.style.backgroundPosition = `center ${scrollPosition * 0.5}px`; // Εφέ parallax
-});
